221290b8eae39e5f57f7db96b0cf1ad19106ce70
Prince Rawat <108456208+Kashina69@users.noreply.github.com>
2024-05-16 11:34:09 +0530

    Add files via upload

--- content.js
+++ content.js
@@ -1,18 +1,16 @@
-// Wait for the page to fully load
+
 window.addEventListener('load', () => {
-    // Check every 2 seconds if there is an ad
     setInterval(() => {
       skipAd();
-    }, 500);
+    }, 1000);
   });
   
   function skipAd() {
-    const adOverlay = document.querySelector('.ytp-ad-overlay-close-button');
-    const skipButton = document.querySelector('.ytp-ad-skip-button-modern ');
-    if (adOverlay) {
-      adOverlay.click(); // Close ad overlay
-    }
+    const skipButton = document.querySelector('.ytp-skip-ad-button');
     if (skipButton) {
-      skipButton.click(); // Click skip ad button
+      skipButton.click();
     }
   }
+
+
+
