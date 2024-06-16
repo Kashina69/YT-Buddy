f4f5344c2a5cf8563bd9a946687ff3d067ad5a86
Prince Rawat <108456208+Kashina69@users.noreply.github.com>
2024-06-16 23:27:39 +0530

    Add files via upload

--- background.js
+++ background.js
@@ -20,6 +20,24 @@ chrome.tabs.onActivated.addListener(() => {
   }
 });
 
+chrome.windows.onFocusChanged.addListener((windowId) => {
+  if (windowId === chrome.windows.WINDOW_ID_NONE) {
+    // Chrome window is not focused
+    chrome.tabs.query({}, (tabs) => {
+      tabs.forEach((tab) => {
+        chrome.tabs.sendMessage(tab.id, { action: "pause" });
+      });
+    });
+  } else {
+    // Chrome window is focused
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0]) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "play" });
+      }
+    });
+  }
+});
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "toggle") {
     isExtensionEnabled = request.value;
--- content.js
+++ content.js
@@ -1,27 +1,31 @@
 window.addEventListener('load', () => {
-    setInterval(() => {
-      skipAd();
-    }, 1000);
-  });
-  
-  function skipAd() {
-    const skipButton = document.querySelector('.ytp-skip-ad-button');
-    if (skipButton) {
-      skipButton.click();
-    }
+  setInterval(() => {
+    skipAd();
+  }, 1000);
+});
+
+function skipAd() {
+  const skipButton = document.querySelector('.ytp-skip-ad-button');
+  if (skipButton) {
+    skipButton.click();
   }
+}
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "pause") {
-    document.querySelectorAll('video').forEach((video) => {
-      video.pause();
-    });
-  } else if (request.action === "play") {
+    if (!window.location.href.includes("music.youtube.com")) {
+      document.querySelectorAll('video').forEach((video) => {
+        video.pause();
+      });
+    }
+  }
+  if (window.location.href.includes("music.youtube.com")) {
+    return;
+  }
+  else if (request.action === "play") {
+
     document.querySelectorAll('video').forEach((video) => {
       video.play();
     });
   }
 });
-
-
-
